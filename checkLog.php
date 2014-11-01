f422f83cff956d9b919b91769869695e48610f3e
Augustin <augustin.chini@hotmail.fr>
2014-11-01 13:01:05 +0100

    fix the 2 failures in login_test_chech.php
    bool error return and unused var $charset

--- checkLog.php
+++ checkLog.php
@@ -1,7 +1,7 @@
 <?php 
 	function checkLog($log, $fil)
 	{
-		$result = false;
+		$result = 'err';
 		if(!empty($log) && !empty($fil) && isset($log) && isset($fil) && strlen($log) > 2 && strlen($log)< 15)
 		{
 			$log = stripForbiddenCharacters($log);
@@ -12,7 +12,7 @@
 	}
 	
 	function stripForbiddenCharacters($str){
-		$str = htmlentities($str, ENT_NOQUOTES, $charset);
+		$str = htmlentities($str, ENT_NOQUOTES);
     	$str = preg_replace('#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str);
 	    $str = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $str); 
     	$str = preg_replace('#&[^;]+;#', '', $str); 
